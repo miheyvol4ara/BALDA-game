94f6b32202fe9cbd9dfe416e0c368ffc0bf2a5da
miheyvol4ara <vol4ara2012@mail.ru>
2012-12-12 23:00:38 +0200

    Update Балда/Program.cs

--- Балда/Program.cs
+++ Балда/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Балда
+namespace балд
 {
     static class Program
     {
